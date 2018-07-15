41c538e5b77cea52f29b77566e2d7511eea783ee
Emerson Cedeno <ecedenyo@gmail.com>
2018-07-14 23:28:31 -0500

    Expandiendo las búsquedas mediante Laravel Scout con Algolia a incluir el nombre de los autores de mensajes, se aprovecha y mejora diseño de pantalla de resultados.

--- app/Message.php
+++ app/Message.php
@@ -8,7 +8,7 @@ use Laravel\Scout\Searchable;
 class Message extends Model
 {
     use Searchable;
-    
+
     protected $guarded = [];
 
     public function user()
@@ -24,5 +24,12 @@ class Message extends Model
 
     	return \Storage::disk('public')->url($image);
     	
+    }
+
+    public function toSearchableArray()
+    {
+        $this->load('user');
+
+        return $this->toArray();
     }
 }
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 
+<div class="row">
 @foreach($messages as $message)
-	@include('messages.message')
+	<div class="col-3">@include('messages.message')</div>
 @endforeach
+</div>
 
 @if(count($messages)) 
 <div class="mt-5 mb-5 mx-auto">{{ $messages->links() }}</div>
